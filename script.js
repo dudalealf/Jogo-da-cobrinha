b42af144c2145e7d6278e421cd82c93a53494ab9
unknown <madulealferreira.2002@gmail.com>
2021-03-12 22:26:07 -0300

    fim do jogo

--- script.js
+++ script.js
@@ -47,6 +47,15 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++)
+    {
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y)
+        {
+            clearInterval(jogo);
+            alert(" G A M E O V E R :(");
+        }
+    }
+
     criarBG();
     criaCobrinha();
     drawFood();
